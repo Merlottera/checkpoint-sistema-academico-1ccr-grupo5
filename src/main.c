52154736c6b9bffe88ab73840ef07c7b3b91a3b1
yKushz <galf.ethicalhacker@gmail.com>
2026-04-22 22:13:37 -0300

    Cria metodo para exibir inicio do programa

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
+
+void console(void){
+    printf("==============================\n");  
+    printf("     SISTEMA EQUIPE STIG4     \n"); 
+    printf("==============================\n"); // Adicionando exibição do nome do programa
+}
+
+
+
+
 int main() {
     
 
