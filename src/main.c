f49623eee50423cce25f4b94a77ceee23c0016b0
yKushz <galf.ethicalhacker@gmail.com>
2026-04-22 22:24:55 -0300

    Adiciona opcoes para usuario escolher

--- src/main.c
+++ src/main.c
@@ -5,6 +5,14 @@ void console(void){
     printf("==============================\n");  
     printf("     SISTEMA EQUIPE STIG4     \n"); 
     printf("==============================\n"); // Adicionando exibição do nome do programa
+    printf("1 - Inserir notas\n");
+    printf("2 - Calcular media\n");
+    printf("3 - Verificar situacao\n");
+    printf("4 - Exibir resultado\n");
+    printf("5 - Calcular derivada\n");
+    printf("6 - Sair\n");
+    printf("Escolha uma opcao:\n");
+    // Adicionando opcoes para usuario escolher
 }
 
 
@@ -12,7 +20,7 @@ void console(void){
 
 int main() {
     
-
+    console();
     
     return 0;
 }
