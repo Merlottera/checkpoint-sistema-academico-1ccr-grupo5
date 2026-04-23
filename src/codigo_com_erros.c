3e9e973eae2f15bbeef77d5a4bd9fa0632dfbc45
Thiago Renatino Paulino <thiagorenatino@gmail.com>
2026-04-22 22:03:07 -0300

    Adiciona validação do input das variáveis nota1 e nota2

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -6,10 +6,20 @@ int main() {
     printf("Digite a primeira nota: ");
     scanf("%f", &nota1);
 
+    if (nota1 < 0 || nota1 > 10) { // Adiciona validação do input das variáveis nota1 e nota2
+        printf("Digite uma nota entre 0 e 10.\n");
+        return 0;
+    }
+
     printf("Digite a segunda nota: ");
     scanf("%f", &nota2); // Adiciona & para indicar corretamente o ponteiro nota2
 
-    media = (nota1 + nota2) / 2; // Adiciona parênteses para cálculo correto da média
+    if (nota2 < 0 || nota2 > 10) {
+        printf("Digite uma nota entre 0 e 10.\n");
+        return 0;
+    }
+
+    media = (nota1 + nota2) / 2; // Adiciona parênteses para cálculo correto da média    
 
     if (media >= 6) { // Corrige uso correto do operador de comparação
         printf("Aluno aprovado\n");
